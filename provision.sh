96d7de188394507dc8f06bf7b2f44e20c073eb9b
Justin LaPolla <j.a.lapolla@gmail.com>
2016-05-21 15:47:31 -0700

    Complete basic build system
    - So far it installs GMTK and a custom .bashrc file.

--- provision.sh
+++ provision.sh
@@ -11,12 +11,14 @@ PATH="$HOME/usr/local/bin:$PATH"
 export MAKE_PATH="$HOME/usr/local/gmtk/include"
 
 # Install Git. The provisioning system needs Git installed.
-sudo yum install git
+sudo yum -y install git
 
 # Extract provisioning system files
 tar -xzf /vagrant/setup-files.tar.gz
 
-# Run provisioning system in a subshell
+# Run provisioning system in a subshell.  The first subshell installs GMTK on
+# the VM. GMTK is needed for the rest of the provisioning system to function
+# properly.
 (
 
   # Stop execution on error
@@ -25,6 +27,18 @@ tar -xzf /vagrant/setup-files.tar.gz
   cd setup-files/
 
   make install-gmtk
+)
+
+# The second subshell runs the entire provisioning system, now that GMTK is
+# installed. We need to run this in a separate subshell in order for the system
+# to recognize the newly installed GMTK files.
+(
+
+  # Stop execution on error
+  set -e
+
+  cd setup-files/
+
   make all
 )
 
